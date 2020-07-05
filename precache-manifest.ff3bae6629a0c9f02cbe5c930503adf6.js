3a822027aa2027bb0ae80b0e4dd349e07bae687b
Anadi9 <anadithakur99@gmail.com>
2020-07-05 17:24:40 +0530

    Updates

---
+++ precache-manifest.ff3bae6629a0c9f02cbe5c930503adf6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4472d21e1931db6e1ea1",
+    "url": "/Portfolio/static/css/main.7bc5e2b1.chunk.css"
+  },
+  {
+    "revision": "4472d21e1931db6e1ea1",
+    "url": "/Portfolio/static/js/main.8ad4742a.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "c758efb4b28d1b6ef837",
+    "url": "/Portfolio/static/css/2.4ff080c6.chunk.css"
+  },
+  {
+    "revision": "c758efb4b28d1b6ef837",
+    "url": "/Portfolio/static/js/2.eabc2757.chunk.js"
+  },
+  {
+    "revision": "30f0612563b023ef9ae9100dcf0d1172",
+    "url": "/Portfolio/static/media/Anadi1_dp.30f06125.png"
+  },
+  {
+    "revision": "a3d06ba1f9bea3a36421dba6b9986970",
+    "url": "/Portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.0324785c90e514da3cfea8bbc3022cc1.js"
+  "/Portfolio/precache-manifest.ff3bae6629a0c9f02cbe5c930503adf6.js"
 );
 
 workbox.clientsClaim();
