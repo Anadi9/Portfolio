9c70ffde90e40c5cd65c85032c1403dcdbf15002
Anadi9 <anadithakur99@gmail.com>
2020-07-05 17:25:33 +0530

    fixes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'animejs/lib/anime.es.js';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
